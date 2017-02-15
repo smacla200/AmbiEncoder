ac4ba2facc5ccc7815cc11c6854e6e92e63ff0e8
smacla200 <smacla200@lab-m329-mac-13.enterprise.gcal.ac.uk>
2017-02-15 13:26:23 +0000

    b-format commit

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -118,11 +118,12 @@ bool AmbiEncoderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layou
 }
 #endif
 
-void AmbiEncoderAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+void AmbiEncoderAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer&
+                                              midiMessages)
 {
-    const int totalNumInputChannels  = getTotalNumInputChannels();
+    const int totalNumInputChannels = getTotalNumInputChannels();
     const int totalNumOutputChannels = getTotalNumOutputChannels();
-
+    const int numSamples = buffer.getNumSamples();
     // In case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
@@ -131,14 +132,25 @@ void AmbiEncoderAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuf
     // this code if your algorithm always overwrites all the output channels.
     for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    // Convert pan position in degrees to radians
+    float azimuth = (M_PI/180.f) * panPosition;
+    
+    // Get a pointer to each of the Ambisonic channels
+    float* channelDataW = buffer.getWritePointer (0);
+    float* channelDataX = buffer.getWritePointer (1);
+    float* channelDataY = buffer.getWritePointer (2);
+    float* channelDataZ = buffer.getWritePointer (3); // Not used
+    
+    // Loop through each sample
+    for (int i = 0; i < numSamples; i++)
     {
-        float* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
+        float audioIn = channelDataW[i]; // Store input in temp variable
+        
+        // Do the encoding (horizontal only)
+        channelDataW[i] = audioIn * 0.707;
+        channelDataX[i] = audioIn * cos(azimuth);
+        channelDataY[i] = audioIn * sin(azimuth);
+        
     }
 }
 
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,7 +56,7 @@ public:
     void getStateInformation (MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
     
-    float panPosition; 
+    float panPosition;
 
 private:
     //==============================================================================
